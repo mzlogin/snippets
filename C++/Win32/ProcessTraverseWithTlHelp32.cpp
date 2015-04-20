ff7967ccd3520eeddba9725f23439abb12bc6b49
Zhuang Ma <ChumpMa@gmail.com>
2015-04-20 15:31:39 +0800

    update process traverse

--- C++/Win32/ProcessTraverseWithTlHelp32.cpp
+++ C++/Win32/ProcessTraverseWithTlHelp32.cpp
@@ -9,15 +9,17 @@ void TraverseProcessWithTlHelp32()
 	{
 		_tprintf(_T("CreateToolhelp32Snapshot failed\n"));
 	}
-
-	PROCESSENTRY32 pe = {0};
-	pe.dwSize = sizeof(PROCESSENTRY32);
-	BOOL bRet = Process32First(hSnapshot, &pe);
-	while (bRet)
-	{
-		_tprintf(_T("PID is %d, PNAME is %s\n"), pe.th32ProcessID, pe.szExeFile);
-		bRet = Process32Next(hSnapshot, &pe);
-	}
+    else
+    {
+        PROCESSENTRY32 pe = {0};
+        pe.dwSize = sizeof(PROCESSENTRY32);
+        BOOL bRet = Process32First(hSnapshot, &pe);
+        while (bRet)
+        {
+            _tprintf(_T("PID is %d, PNAME is %s\n"), pe.th32ProcessID, pe.szExeFile);
+            bRet = Process32Next(hSnapshot, &pe);
+        }
+    }
 }
 
 int _tmain(int argc, _TCHAR* argv[])
