6d51262e3d65ad8892633223756d403dd82364bb
Zhuang Ma <ChumpMa@gmail.com>
2015-07-02 19:52:06 +0800

    update django demo

---
+++ Python/django/mysite/polls/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<poll_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<poll_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<poll_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
--- Python/django/mysite/polls/views.py
+++ Python/django/mysite/polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
