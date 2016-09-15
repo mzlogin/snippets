22e1001bf473e9ceb0637397601038238e07a571
Zhuang Ma <chumpma@gmail.com>
2016-09-15 11:00:20 +0800

    Add remove from List method

--- Java/src/CollectionsOperate.java
+++ Java/src/CollectionsOperate.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by mazhuang on 2016/9/13.
@@ -8,6 +6,7 @@ import java.util.Map;
 public class CollectionsOperate {
     public static void main(String[] args) {
         deleteFromMap();
+        deleteFromList();
     }
 
     public static void deleteFromMap() {
@@ -19,15 +18,46 @@ public class CollectionsOperate {
         // ok, but not in traversal
         map.remove(1);
 
+        // HashMap doesn't implements Interable interface, so cannot use for-each directly
         for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            // error! java.util.ConcurrentModificationException
+            // error! java.util.ConcurrentModificationException if not break
             // map.remove(entry.getKey());
+            // break;
         }
 
         Iterator<Map.Entry<Integer, String>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
             // the only right way to remove element from collections when traversal
             iterator.next();
+            iterator.remove();
+        }
+    }
+
+    public static void deleteFromList() {
+        String[] array = {"Hello", "World"};
+        List<String> list = Arrays.asList(array); // !!! Arrays$ArrayList<>, it's different from ArrayList<>
+
+        for (String str : list) {
+            // error! java.lang.UnsupportedOperationException
+            // Arrays$ArrayList<>.remove not implemented
+            // list.remove(0);
+        }
+
+        list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        list.add("Hi");
+
+        for (String str : list) {
+            // error! java.util.ConcurrentModificationException if not break
+            // list.remove(str);
+            // break;
+        }
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            // java.lang.IllegalStateException without iterator.next() before
             iterator.remove();
         }
     }
