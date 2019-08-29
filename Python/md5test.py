db8c6a3b00c6ff8a2922134586739b0d25ec9f06
Zhuang Ma <chumpma@gmail.com>
2019-08-29 16:44:48 +0800

    Update md5 via python

--- Python/md5test.py
+++ Python/md5test.py
@@ -1,16 +1,24 @@
-import md5
-import ctypes
+import hashlib
 
-def str_md5(str1):
-    m = md5.new()
-    m.update(str1)
-    return m.hexdigest()
+while True:
+    s = input('input string to calc md5: ')
+    hl = hashlib.md5()
+    hl.update(s.encode('utf-8'))
+    print(hl.hexdigest())
 
-if __name__ == '__main__':
-    while True:
-        str1 = raw_input('input string to calc md5: ')
-        str1md5 = str_md5(str1)
-        print 'md5 is: ' + str1md5
-        high16 = str1md5[0:16]
-        print 'high16 raw is: ' + high16
-        print 'high16 is: %d' % ctypes.c_int64(int(high16, 16)).value
+# import md5
+# import ctypes
+
+# def str_md5(str1):
+#     m = md5.new()
+#     m.update(str1)
+#     return m.hexdigest()
+
+# if __name__ == '__main__':
+#     while True:
+#         str1 = raw_input('input string to calc md5: ')
+#         str1md5 = str_md5(str1)
+#         print 'md5 is: ' + str1md5
+#         high16 = str1md5[0:16]
+#         print 'high16 raw is: ' + high16
+#         print 'high16 is: %d' % ctypes.c_int64(int(high16, 16)).value
