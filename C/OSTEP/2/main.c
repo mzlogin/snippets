84d11e14ecd175a633b21ca4d8f6f2ff22a78aa7
Zhuang Ma <chumpma@gmail.com>
2020-02-01 13:10:16 +0800

    add snippets from OSTEP

---
+++ C/OSTEP/2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) {
+    int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+    assert(fd > -1);
+    int rc = write(fd, "hello world\n", 13);
+    assert(rc == 13);
+    close(fd);
+
+    return 0;
+}
